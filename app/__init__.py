2f74e45f1424c34c27dae7ec2b9aa6a79c04ecc6
marco <77583303+Marco-374@users.noreply.github.com>
2024-11-27 17:17:42 +0100

    feat: setup config for unit-testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,25 @@
 from flask import Flask
 from dotenv import load_dotenv
 
-from .config import Config
 from flask_migrate import Migrate
 
 
-def create_app():
+def create_app(config_class):
     app = Flask(__name__)
     load_dotenv()
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     # Register blueprints
-    from .routes import main
+    from .routes import main, project
 
     app.register_blueprint(main)
 
-    from app.db import db
+    if not config_class.TESTING:
+        from app.db import db
 
-    db.init_app(app)
-    Migrate(app, db)
-    with app.app_context():
-        db.create_all()
+        db.init_app(app)
+        Migrate(app, db)
+        with app.app_context():
+            db.create_all()
 
     return app
