67e3094e749041247b7c70b029a87cba97d462fb
marco <77583303+Marco-374@users.noreply.github.com>
2025-01-03 13:14:05 +0100

    feat: authorization in swagger docu possible

--- app/extension.py
+++ app/extension.py
@@ -3,9 +3,18 @@ from flask_restx import Api
 
 
 main = Blueprint("main", __name__, url_prefix="/api")
+authorizations = {
+    "apikey": {
+        "type": "apiKey",
+        "in": "header",
+        "name": "Authorization",
+        "description": "Type in the *'Value'* input box below: **'Bearer &lt;JWT&gt;'**, where JWT is the token",
+    }
+}
 api = Api(
     main,
     title="Annotation Project Backend API",
     version="1.0",
     doc="/docs",
+    authorizations=authorizations,
 )
