6fbac4bd8889fe560bb414a070985cb02b6a9b87
marco <77583303+Marco-374@users.noreply.github.com>
2025-01-03 13:27:56 +0100

    fix: swagger authorization

--- app/extension.py
+++ app/extension.py
@@ -4,7 +4,7 @@ from flask_restx import Api
 
 main = Blueprint("main", __name__, url_prefix="/api")
 authorizations = {
-    "apikey": {
+    "Bearer": {
         "type": "apiKey",
         "in": "header",
         "name": "Authorization",
@@ -16,5 +16,6 @@ api = Api(
     title="Annotation Project Backend API",
     version="1.0",
     doc="/docs",
+    security="Bearer",
     authorizations=authorizations,
 )
