ada6155497ff79cbae9e1eb467107abdeabb2395
Niklas2610 <niklas959390@gmail.com>
2025-01-14 16:44:23 +0100

    Update relation_repository.py

--- app/repositories/relation_repository.py
+++ app/repositories/relation_repository.py
@@ -97,12 +97,18 @@ class RelationRepository(BaseRepository):
 
 
 
-    def update_is_shown_recommendation(self, relation_id, value):
-        """
-        Aktualisiert den isShownRecommendation-Wert eines Relation-Eintrags.
-        """
-        relation = self.db_session.query(Relation).filter_by(id=relation_id).first()
-        if relation:
-            relation.isShownRecommendation = value
-            self.db_session.commit()
+    def update_relation(
+        self, relation_id, tag, mention_head_id, mention_tail_id, is_directed
+    ):
+        relation = self.get_relation_by_id(relation_id)
+        if tag:
+            relation.tag = tag
+        if mention_head_id:
+            relation.mention_head_id = mention_head_id
+        if mention_tail_id:
+            relation.mention_tail_id = mention_tail_id
+        if is_directed is not None:
+            relation.isDirected = is_directed
+
+        super().store_object(relation)
         return relation
