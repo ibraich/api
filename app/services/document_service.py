9bc6fa16f1982a351296e99a5a62ac6df0213acf
L-Moritz <moritz.lunk@t-online.de>
2025-01-30 14:48:12 +0100

    feat: adjust endpoints to enable specific frontend functionality

--- app/services/document_service.py
+++ app/services/document_service.py
@@ -118,15 +118,19 @@ class DocumentService:
             )
         )
         if not document_edits:
-            raise NotFound("No DocumentEdits found for document ID")
+            raise NotFound(f"No DocumentEdits found for document ID #{document_id}")
 
         processed_edits = [
             {
-                "id": document_id,
+                "id": edit.edit_id,
                 "user": {
                     "id": edit.user_id,
                     "email": edit.user_email,
                     "username": edit.user_username,
+                },
+                "state": {
+                    "id": edit.state_id,
+                    "type": edit.state_type,
                 },
             }
             for edit in document_edits
@@ -159,6 +163,7 @@ class DocumentService:
                 "id": doc.document_edit_id,
                 "state": doc.document_edit_state,
             },
+            "document_edits": self.get_all_document_edits_with_user_by_document(doc.id),
             "creator": {
                 "id": doc.creator_id,
                 "username": doc.username,
