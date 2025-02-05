a1ffa5b8c53f6d508ad12c9aadbae366dc77593f
kanwal.gul <kanwal.gul@medznmore.com>
2025-02-05 21:31:49 +0100

    feat: add document edit route

--- app/routes/document_edit_routes.py
+++ app/routes/document_edit_routes.py
@@ -12,6 +12,7 @@ from app.dtos import (
     document_edit_output_soft_delete_dto,
     finished_document_edit_output_dto,
     document_edit_model_output_list_dto,
+    f1_score_dto,
 )
 
 ns = Namespace("annotations", description="Document-Annotation related operations")
@@ -116,3 +117,17 @@ class DocumentEditResource(DocumentEditBaseRoute):
 
         response = self.service.get_document_edit_model(document_edit_id)
         return response
+
+
+@ns.route("/<int:document_edit_id>/f1score")
+@ns.doc(params={"document_edit_id": "A Document ID"})
+@ns.response(403, "Authorization required")
+@ns.response(404, "Data not found")
+class DocumentEditF1Score(DocumentEditBaseRoute):
+
+    @ns.marshal_with(f1_score_dto)
+    @ns.doc(description="Get F1 score for document edit")
+    def get(self, document_edit_id):
+        user_id = self.user_service.get_logged_in_user_id()
+        self.user_service.check_user_document_edit_accessible(user_id, document_edit_id)
+        return self.service.get_f1_score(document_edit_id)
